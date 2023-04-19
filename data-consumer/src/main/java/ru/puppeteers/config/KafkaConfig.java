a2d6527c88d718e06378b7d3c4d50a6d0486b426
maratsadykov <marats93@yandex.ru>
2023-04-19 21:52:15 +0300

    fixes

--- data-consumer/src/main/java/ru/puppeteers/config/KafkaConfig.java
+++ data-consumer/src/main/java/ru/puppeteers/config/KafkaConfig.java
@@ -5,18 +5,14 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
-import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
-import reactor.kafka.receiver.internals.ConsumerFactory;
-import reactor.kafka.receiver.internals.DefaultKafkaReceiver;
 
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class KafkaConfig {
 
-    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    @Value("${spring.kafka.template.default-topic}")
     private String bootstrapServer;
 
     @Bean
@@ -31,15 +27,4 @@ public class KafkaConfig {
             ReceiverOptions<String, String> kafkaReceiverOptions) {
         return new ReactiveKafkaConsumerTemplate<>(kafkaReceiverOptions);
     }
-
-//    @Bean
-//    public KafkaReceiver<String, String> kafkaReceiver(KafkaProperties kafkaProperties) {
-//        kafkaProperties.setBootstrapServers(List.of(bootstrapServer));
-//        kafkaProperties.getConsumer().setGroupId("group1");
-//        ReceiverOptions<String, String> basicReceiverOptions =
-//                ReceiverOptions.create(kafkaProperties.buildConsumerProperties());
-//        basicReceiverOptions.subscription(Collections.singletonList("balance_change"));
-//
-//        return new DefaultKafkaReceiver<>(ConsumerFactory.INSTANCE, basicReceiverOptions);
-//    }
 }
--- data-consumer/src/main/java/ru/puppeteers/service/ConsumerService.java
+++ data-consumer/src/main/java/ru/puppeteers/service/ConsumerService.java
@@ -6,8 +6,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.kafka.receiver.KafkaReceiver;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +18,5 @@ public class ConsumerService {
         return kafkaConsumerTemplate.receiveAutoAck()
                 .doOnNext(consumerRecord -> log.info("Consumed message {}", consumerRecord.value()))
                 .map(ConsumerRecord::value);
-//        return kafkaReceiver.receive()
-//                .map(ConsumerRecord::value)
-//                .doOnEach(message -> log.info("Consumed message {}", message))
-//                .onErrorResume(e -> Mono.just("Exception"));
     }
 }
